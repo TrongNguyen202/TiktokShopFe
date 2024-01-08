123f383db23aead292542b6077e479e89ab1a50e
Duong Minh Quan <hokage321xxx@gmail.com>
2024-01-09 00:54:53 +0700

    style: autopep8 formating

--- api/ultil/tiktokApi.py
+++ api/ultil/tiktokApi.py
@@ -1,11 +1,13 @@
-from .constant import secret,TIKTOK_API_URL,app_key,grant_type
-from api.helpers import GenerateSign,GenerateSignNoBody
+from .constant import secret, TIKTOK_API_URL, app_key, grant_type
+from api.helpers import GenerateSign, GenerateSignNoBody
 import requests
 import json
 import urllib.parse
 
 SIGN = GenerateSign()
-SIGNNOBODY =GenerateSignNoBody()
+SIGNNOBODY = GenerateSignNoBody()
+
+
 def callProductList(access_token):
     url = TIKTOK_API_URL['url_product_list']
     query_params = {
@@ -22,38 +24,40 @@ def callProductList(access_token):
 
     sign = SIGN.cal_sign(secret, urllib.parse.urlparse(url), query_params, body)
     query_params["sign"] = sign
-    
+
     response = requests.post(url, params=query_params, json=json.loads(body))
-  
+
     # Process the response
     print(response.status_code)
     print(response.text)
     return response
 
+
 def getAccessToken(auth_code):
     url = TIKTOK_API_URL['url_get_access_token']
-  
+
     body = json.dumps({
 
-       "app_key":"6atknvel13hna",
-       "app_secret":"df329e59a6f78121409d77c33ee1decfbfa088a4",
-       "grant_type":"authorized_code",
-       "auth_code":auth_code,
+        "app_key": "6atknvel13hna",
+        "app_secret": "df329e59a6f78121409d77c33ee1decfbfa088a4",
+        "grant_type": "authorized_code",
+        "auth_code": auth_code,
     })
 
     response = requests.post(url, json=json.loads(body))
     print(response.status_code)
     print(response.text)
     return response
+
 
 def refreshToken(refreshToken):
     url = TIKTOK_API_URL['url_refresh_token']
     body = json.dumps({
 
-       "app_key":app_key,
-       "app_secret":secret,
-       "refresh_token":refreshToken,
-       "grant_type":grant_type,
+        "app_key": app_key,
+        "app_secret": secret,
+        "refresh_token": refreshToken,
+        "grant_type": grant_type,
     })
     response = requests.post(url, json=json.loads(body))
     print(response.status_code)
@@ -61,21 +65,20 @@ def refreshToken(refreshToken):
     return response
 
 
-def callProductDetail(access_token,product_id):
+def callProductDetail(access_token, product_id):
     url = TIKTOK_API_URL['url_detail_product']
     query_params = {
         "app_key": "6atknvel13hna",
         "access_token": access_token,
         "timestamp": SIGN.get_timestamp(),
-        "product_id":product_id,
+        "product_id": product_id,
     }
-   
 
     sign = SIGNNOBODY.cal_sign(secret, urllib.parse.urlparse(url), query_params)
     query_params["sign"] = sign
-    
+
     response = requests.get(url, params=query_params)
-  
+
     # Process the response
     print(response.status_code)
     print(response.text)
@@ -89,13 +92,13 @@ def callProductDetail(access_token,product_id):
 #         "access_token": access_token,
 #         "timestamp": SIGN.get_timestamp()
 #     }
-   
+
 
 #     sign = SIGNNOBODY.cal_sign(secret, urllib.parse.urlparse(url), query_params )
 #     query_params["sign"] = sign
-    
+
 #     response = requests.post(url, params=query_params)
-  
+
 #     # Process the response
 #     print(response.status_code)
 #     print(response.text)
@@ -107,18 +110,18 @@ def getCategories(access_token):
         "app_key": app_key,
         "access_token": access_token,
         "timestamp": SIGNNOBODY.get_timestamp(),
-        
+
     }
-   
 
     sign = SIGNNOBODY.cal_sign(secret, urllib.parse.urlparse(url), query_params)
     query_params["sign"] = sign
-    
+
     response = requests.get(url, params=query_params)
-  
+
     # Process the response
 
     return response
+
 
 def getWareHouseList(access_token):
     url = TIKTOK_API_URL['url_get_warehouse']
@@ -126,42 +129,37 @@ def getWareHouseList(access_token):
         "app_key": app_key,
         "access_token": access_token,
         "timestamp": SIGNNOBODY.get_timestamp(),
-        
+
     }
-   
 
     sign = SIGNNOBODY.cal_sign(secret, urllib.parse.urlparse(url), query_params)
     query_params["sign"] = sign
-    
+
     response = requests.get(url, params=query_params)
-  
+
     # Process the response
 
     return response
-    
 
 
-def callUploadImage(access_token,img_data):
+def callUploadImage(access_token, img_data):
     url = TIKTOK_API_URL['url_upload_image']
     query_params = {
         "app_key": "6atknvel13hna",
         "access_token": access_token,
         "timestamp": SIGN.get_timestamp(),
-       
+
     }
     body = json.dumps({
-      "img_data":img_data,
-      "img_scene": 1
+        "img_data": img_data,
+        "img_scene": 1
 
     })
 
-   
-
     sign = SIGN.cal_sign(secret, urllib.parse.urlparse(url), query_params, body)
     query_params["sign"] = sign
-    
+
     response = requests.post(url, params=query_params, json=json.loads(body))
-    
 
     # Process the response
     print(response.status_code)
