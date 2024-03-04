2f796c8e9a178938d11a2b598179b81a79d13990
namdang <namdang06092001@gmail.com>
2024-03-04 07:43:53 +0700

    fix permission

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -2,14 +2,14 @@ import { permission } from "../constants";
 
 export const hasManagerPermission = () => {
   const user = JSON.parse(localStorage.getItem('user'));
-  if (!user) return false;
+  if (!user || Array.isArray(user.role)) return false;
   if (user.role[0] === permission.MANAGER) return true;
   return false
 }
 
 export const hasSellerPermission = () => {
   const user = JSON.parse(localStorage.getItem('user'));
-  if (!user) return false;
+  if (!user || !Array.isArray(user.role)) return false;
   if (user.role[0] === permission.SELLER) return true;
   return false
 }
