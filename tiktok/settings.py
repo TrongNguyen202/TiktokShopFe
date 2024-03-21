1f96ed4fc23e41e1c8b778c56a55e291ae3ef1ac
Duong Minh Quan <91663460+2uanDM@users.noreply.github.com>
2024-03-22 04:04:39 +0700

    feat/changing debug mode by env

--- tiktok/settings.py
+++ tiktok/settings.py
@@ -15,10 +15,12 @@ from datetime import timedelta
 from pathlib import Path
 
 from decouple import config
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -27,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEV_MODE")
+DEBUG = os.getenv("DEV_MODE")
 
 ALLOWED_HOSTS = []
 
