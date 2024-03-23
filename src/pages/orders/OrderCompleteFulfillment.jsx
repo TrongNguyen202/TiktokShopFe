ad59259fbf97420dd44942050ed8144f0a8676bd
Zoe-997 <thuminh10011997@mail.com>
2024-03-23 10:57:26 +0700

    fix/fulfillment - change package_id to order_id in FlashShip

--- src/pages/orders/OrderCompleteFulfillment.jsx
+++ src/pages/orders/OrderCompleteFulfillment.jsx
@@ -22,10 +22,12 @@ function OrderCompleteFulfillment() {
   const dataTableFlashShip = dataPackage.filter((item) => item.fulfillment_name === 'FlashShip');
   const dataTablePrintCare = dataPackage.filter((item) => item.fulfillment_name === 'PrintCare');
   const ConvertDataTable = (data) => {
-    return data.map((item, index) => ({
+    const result =  data.reverse().map((item, index) => ({
       stt: index + 1,
       ...item,
     }));
+
+    return result;
   };
 
   const handleOrderPackage = (packageId) => {
@@ -68,6 +70,10 @@ function OrderCompleteFulfillment() {
   }
 
   const handleDetailFlashShip = (orderCode) => {
+    if (flashShipToken === null) {
+      setOpenLoginFlashShip(true)
+    }
+
     const onSuccess = (res) => {
       console.log(res);
     }
@@ -79,6 +85,10 @@ function OrderCompleteFulfillment() {
   }
 
   const handleCancelOrderFlashShip = (orderCode) => {
+    if (flashShipToken === null) {
+      setOpenLoginFlashShip(true)
+    }
+
     const data = {
       "orderCodeList": [orderCode],
       "rejectNote": "wrong design"
@@ -211,11 +221,7 @@ function OrderCompleteFulfillment() {
 
     packageFulfillmentCompleted(shopId, onSuccess, onFail);
     getAllOrders(shopId);
-
-    if (flashShipToken === null) {
-      setOpenLoginFlashShip(true)
-    }
-  }, [flashShipToken]);
+  }, []);
 
   return (
     <div className="p-10">
